8b791f3228cec7cb90eb4ee9249b3b83aba3ab21
Sharparam <adam.hellberg95@gmail.com>
2013-02-08 08:40:16 +0100

    Wrote some helper classes.

---
+++ TriEngine2D/Helpers/IO.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace TriEngine2D.Helpers
+{
+	/// <summary>
+	/// Provides various helper functions for doing IO operations.
+	/// </summary>
+	public static class IO
+	{
+		/// <summary>
+		/// Resolves the absolute path from a relative path.
+		/// </summary>
+		/// <param name="path">The relative path to resolve.</param>
+		/// <returns>The absolute path to the item.</returns>
+		public static string GetAbsolutePath(string path)
+		{
+			return Path.Combine(Directory.GetCurrentDirectory(), path);
+		}
+	}
+}
---
+++ TriEngine2D/Helpers/Threading.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+
+namespace TriEngine2D.Helpers
+{
+	/// <summary>
+	/// Provides various helper functions for doing threading operations.
+	/// </summary>
+	public static class Threading
+	{
+		/// <summary>
+		/// Sets the name of the current thread,
+		/// does nothing if the thread already has a name.
+		/// </summary>
+		/// <param name="name">The new name for the current thread</param>
+		public static void SetCurrentThreadName(string name)
+		{
+			// We can't set the name on a thread if it's already set, it would throw an exception
+			// So we have to check if the current name is null before trying to set a new one
+			if (string.IsNullOrEmpty(Thread.CurrentThread.Name))
+				Thread.CurrentThread.Name = name;
+		}
+	}
+}
