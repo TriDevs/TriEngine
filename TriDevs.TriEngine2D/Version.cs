e902ec9bc57225288a12173f6b3ddb273f14b2fc
Sharparam <adam.hellberg95@gmail.com>
2013-02-08 12:02:34 +0100

    Updated version numbers.

--- TriDevs.TriEngine2D/Version.cs
+++ TriDevs.TriEngine2D/Version.cs
@@ -41,7 +41,7 @@ namespace TriDevs.TriEngine2D
 		/// <summary>
 		/// Patch version of the project.
 		/// </summary>
-		public const int Patch = 2;
+		public const int Patch = 3;
 
 		/// <summary>
 		/// Optional suffix, empty if no suffix for this version.
