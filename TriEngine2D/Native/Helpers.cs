dfd1d07eb27b31138b7b2c5cf7a376bff291ff93
Sharparam <adam.hellberg95@gmail.com>
2013-02-08 09:04:55 +0100

    Add logging classes.

---
+++ TriEngine2D/Native/Helpers.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace TriEngine2D.Native
+{
+	/// <summary>
+	/// Helper class with various methods to help native coding and debugging.
+	/// </summary>
+	public static class Helpers
+	{
+		/// <summary>
+		/// Gets the last error that was thrown.
+		/// </summary>
+		/// <returns>The error code associated with the thrown error.</returns>
+		public static int GetLastError()
+		{
+			return Marshal.GetLastWin32Error();
+		}
+
+		/// <summary>
+		/// Gets information about the last error that was thrown.
+		/// </summary>
+		/// <param name="message">Will be set to the error message.</param>
+		/// <returns>The error code associated with the thrown error.</returns>
+		public static int GetLastErrorInfo(out string message)
+		{
+			var err = GetLastError();
+			message = GetWin32Exception(err).Message;
+			return err;
+		}
+
+		/// <summary>
+		/// Gets the error message associated with an error code.
+		/// </summary>
+		/// <param name="err">The error code to get information about.</param>
+		/// <returns>The error message.</returns>
+		public static string GetErrorMessage(int err)
+		{
+			return GetWin32Exception(err).Message;
+		}
+
+		/// <summary>
+		/// Gets the error message associated with the last thrown error.
+		/// </summary>
+		/// <returns>The error message.</returns>
+		public static string GetLastErrorMessage()
+		{
+			return GetErrorMessage(GetLastError());
+		}
+
+		/// <summary>
+		/// Gets the <see cref="Win32Exception" /> associated with the specified error code.
+		/// </summary>
+		/// <param name="err">The error code.</param>
+		/// <returns>The <see cref="Win32Exception" /> for the provided error code.</returns>
+		public static Win32Exception GetWin32Exception(int err)
+		{
+			return new Win32Exception(err);
+		}
+
+		/// <summary>
+		/// Gets the <see cref="Win32Exception" /> associated with the last thrown error.
+		/// </summary>
+		/// <returns>The <see cref="Win32Exception" />.</returns>
+		public static Win32Exception GetLastWin32Exception()
+		{
+			return GetWin32Exception(GetLastError());
+		}
+	}
+}
---
+++ TriEngine2D/Native/WinAPI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TriEngine2D.Native
+{
+	/// <summary>
+	/// Holds various WinAPI stuff.
+	/// </summary>
+	public static class WinAPI
+	{
+// ReSharper disable InconsistentNaming
+		// Functions
+
+		/// <summary>
+		/// Retrieves a handle to the specified standard device
+		/// (standard input, standard output, or standard error).
+		/// </summary>
+		/// <param name="nStdHandle">The standard device.</param>
+		/// <returns>
+		/// If the function succeeds, the return value is a handle to the specified device,
+		/// or a redirected handle set by a previous call to SetStdHandle.
+		/// The handle has GENERIC_READ and GENERIC_WRITE access rights,
+		/// unless the application has used SetStdHandle to set a standard handle with lesser access.
+		/// If the function fails, the return value is INVALID_HANDLE_VALUE.
+		/// To get extended error information, call GetLastError.
+		/// If an application does not have associated standard handles,
+		/// such as a service running on an interactive desktop,
+		/// and has not redirected them, the return value is NULL.
+		/// </returns>
+		[DllImport("kernel32.dll", EntryPoint = "GetStdHandle", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+		public static extern IntPtr GetStdHandle(int nStdHandle);
+
+		/// <summary>
+		/// Allocates a new console for the calling process.
+		/// </summary>
+		/// <returns>
+		/// If the function succeeds, the return value is nonzero.
+		/// If the function fails, the return value is zero. To get extended error information, call GetLastError.
+		/// </returns>
+		[DllImport("kernel32.dll", EntryPoint = "AllocConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+		public static extern bool AllocConsole();
+
+		/// <summary>
+		/// Detaches the calling process from its console.
+		/// </summary>
+		/// <returns>
+		/// If the function succeeds, the return value is nonzero.
+		/// If the function fails, the return value is zero.
+		/// To get extended error information, call GetLastError.
+		/// </returns>
+		[DllImport("kernel32.dll", EntryPoint = "FreeConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+		public static extern int FreeConsole();
+
+		// Constants
+
+		/// <summary>
+		/// The standard output device. Initially, this is the active console screen buffer, CONOUT$.
+		/// </summary>
+		public const int STD_OUTPUT_HANDLE = -11;
+
+		/// <summary>
+		/// The code page to use for the console.
+		/// </summary>
+		public const int CODE_PAGE = 437;
+// ReSharper restore InconsistentNaming
+	}
+}
