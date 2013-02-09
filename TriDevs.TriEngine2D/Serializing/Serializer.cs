d77d1850354173c15d126a52bfe8d5d37fdea21b
Sharparam <adam.hellberg95@gmail.com>
2013-02-09 21:53:05 +0100

    Added serialization.

---
+++ TriDevs.TriEngine2D/Serializing/Serializer.cs
@@ -0,0 +1,91 @@
+﻿/* Serializer.cs
+ *
+ * Copyright © 2013 by Adam Hellberg, Sijmen Schoon and Preston Shumway.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TriDevs.TriEngine2D.Serializing
+{
+	/// <summary>
+	/// Provides serialization methods.
+	/// </summary>
+	public static class Serializer
+	{
+		private static readonly Lazy<JsonSerializer> JsonSerializer = new Lazy<JsonSerializer>();
+
+		/// <summary>
+		/// Serialize an object to string.
+		/// </summary>
+		/// <typeparam name="T">Type of data.</typeparam>
+		/// <param name="data">Data to serialize.</param>
+		/// <returns>The serialized object in string format.</returns>
+		public static string Serialize<T>(T data)
+		{
+			var writer = new StringWriter();
+			JsonSerializer.Value.Serialize(writer, data);
+			return writer.ToString();
+		}
+
+		/// <summary>
+		/// Serializes an object to file.
+		/// </summary>
+		/// <typeparam name="T">Type of the data.</typeparam>
+		/// <param name="data">Data to serialize.</param>
+		/// <param name="file">File to serialize to.</param>
+		/// <param name="formatting">The formatting to use for the JSON output.</param>
+		public static void Serialize<T>(T data, string file, Formatting formatting = Formatting.Indented)
+		{
+			using (var writer = new StreamWriter(file, false))
+			{
+				using (var jsonWriter = new JsonTextWriter(writer){Formatting = formatting})
+				{
+					JsonSerializer.Value.Serialize(jsonWriter, data);
+					jsonWriter.Close();
+				}
+				writer.Close();
+			}
+		}
+
+		/// <summary>
+		/// Deserialize a serialized object from file.
+		/// </summary>
+		/// <typeparam name="T">Type of the object being deserialized.</typeparam>
+		/// <param name="file">File to read from.</param>
+		/// <returns>The deserialized object.</returns>
+		public static T Deserialize<T>(string file)
+		{
+			T data;
+			using (var reader = new StreamReader(file))
+			{
+				using (var jsonReader = new JsonTextReader(reader))
+				{
+					data = JsonSerializer.Value.Deserialize<T>(jsonReader);
+					jsonReader.Close();
+				}
+				reader.Close();
+			}
+			return data;
+		}
+	}
+}
