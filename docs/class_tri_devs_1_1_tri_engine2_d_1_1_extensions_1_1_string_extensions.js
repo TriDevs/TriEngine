49d127d4de38de2cc725fe5fbc05acf94ccfdb20
Sharparam <adam.hellberg95@gmail.com>
2013-02-10 16:38:07 +0100

    Documentation update.

---
+++ docs/class_tri_devs_1_1_tri_engine2_d_1_1_extensions_1_1_string_extensions.js
@@ -0,0 +1,6 @@
+var class_tri_devs_1_1_tri_engine2_d_1_1_extensions_1_1_string_extensions =
+[
+    [ "Replace", "class_tri_devs_1_1_tri_engine2_d_1_1_extensions_1_1_string_extensions.html#af13fed608d3d1dbd6d9adff7acb177a2", null ],
+    [ "Replace", "class_tri_devs_1_1_tri_engine2_d_1_1_extensions_1_1_string_extensions.html#a893fc7ddd1bfad51febc3db33a163edc", null ],
+    [ "ReplaceFirst", "class_tri_devs_1_1_tri_engine2_d_1_1_extensions_1_1_string_extensions.html#ae01a3219ba8f9715bdfe813b5ecd095c", null ]
+];
--- docs/namespace_tri_devs_1_1_tri_engine2_d_1_1_extensions.js
+++ docs/namespace_tri_devs_1_1_tri_engine2_d_1_1_extensions.js
@@ -1,4 +1,5 @@
 var namespace_tri_devs_1_1_tri_engine2_d_1_1_extensions =
 [
-    [ "EnumerationExtensions", "class_tri_devs_1_1_tri_engine2_d_1_1_extensions_1_1_enumeration_extensions.html", "class_tri_devs_1_1_tri_engine2_d_1_1_extensions_1_1_enumeration_extensions" ]
+    [ "EnumerationExtensions", "class_tri_devs_1_1_tri_engine2_d_1_1_extensions_1_1_enumeration_extensions.html", "class_tri_devs_1_1_tri_engine2_d_1_1_extensions_1_1_enumeration_extensions" ],
+    [ "StringExtensions", "class_tri_devs_1_1_tri_engine2_d_1_1_extensions_1_1_string_extensions.html", "class_tri_devs_1_1_tri_engine2_d_1_1_extensions_1_1_string_extensions" ]
 ];
