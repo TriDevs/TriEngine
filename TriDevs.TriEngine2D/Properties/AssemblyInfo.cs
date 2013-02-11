5a416e1799fe91cc945cef94e4282d7d8e03330a
Sharparam <adam.hellberg95@gmail.com>
2013-02-11 12:25:50 +0100

    Enabled NuGet package restore.

--- TriDevs.TriEngine2D/Properties/AssemblyInfo.cs
+++ TriDevs.TriEngine2D/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.26.0124")]
+[assembly: AssemblyFileVersion("1.0.29.1223")]
