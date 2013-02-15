d4aa68695a56515c4e17b694890a78d1f9513143
Sharparam <adam.hellberg95@gmail.com>
2013-02-15 20:52:49 +0100

    Worked some more on the input classes, added events.

---
+++ TriDevs.TriEngine2D/Input/Events/Mouse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Input;
+
+namespace TriDevs.TriEngine2D.Input.Events
+{
+    public delegate void MouseDownEventHandler(object sender, MouseButtonEventArgs e);
+
+    public delegate void MouseUpEventHandler(object sender, MouseButtonEventArgs e);
+
+    public delegate void MouseWheelChangedEventHandler(object sender, MouseWheelEventArgs e);
+
+    public delegate void MouseWheelDownEventHandler(object sender, MouseWheelEventArgs e);
+
+    public delegate void MouseWheelUpEventHandler(object sender, MouseWheelEventArgs e);
+}
--- TriDevs.TriEngine2D/Input/InputManager.cs
+++ TriDevs.TriEngine2D/Input/InputManager.cs
@@ -23,6 +23,7 @@
 
 using OpenTK;
 using OpenTK.Input;
+using TriDevs.TriEngine2D.Input.Events;
 
 namespace TriDevs.TriEngine2D.Input
 {
@@ -31,6 +32,47 @@ namespace TriDevs.TriEngine2D.Input
     /// </summary>
     public class InputManager : IInputManager
     {
+        /// <summary>
+        /// Raised when a key is pressed down.
+        /// </summary>
+        public event KeyDownEventHandler KeyDown;
+
+        /// <summary>
+        /// Raised when a key is released.
+        /// </summary>
+        public event KeyUpEventHandler KeyUp;
+
+        /// <summary>
+        /// Raised when a character is typed.
+        /// </summary>
+        public event KeyPressEventHandler KeyPress;
+
+        /// <summary>
+        /// Raised when a mouse button is pressed down.
+        /// </summary>
+        public event MouseDownEventHandler MouseDown;
+
+        /// <summary>
+        /// Raised when a mouse button is released.
+        /// </summary>
+        public event MouseUpEventHandler MouseUp;
+
+        /// <summary>
+        /// Raised when the mouse wheel value changes.
+        /// </summary>
+        public event MouseWheelChangedEventHandler WheelChanged;
+
+        /// <summary>
+        /// Raised when the mouse wheel is scrolled downwards.
+        /// </summary>
+        public event MouseWheelDownEventHandler WheelDown;
+
+        /// <summary>
+        /// Raised when the mouse wheel is scrolled upwards.
+        /// </summary>
+        public event MouseWheelUpEventHandler WheelUp;
+
+        private readonly KeyboardDevice _keyboard;
         private readonly MouseDevice _mouse;
 
         private KeyboardState _keyboardState;
@@ -38,7 +80,7 @@ namespace TriDevs.TriEngine2D.Input
 
         private MouseState _mouseState;
         private MouseState _lastMouseState;
-
+        
         public int MouseX { get { return _mouse.X; } }
         public int MouseY { get { return _mouse.Y; } }
         public Point<int> MousePosition { get { return new Point<int>(MouseX, MouseY); } }
@@ -55,12 +97,90 @@ namespace TriDevs.TriEngine2D.Input
         }
 
         /// <summary>
+        /// Creates a new <see cref="InputManager" /> with only basic low-level input support.
+        /// </summary>
+        /// <remarks>
+        /// Creating <see cref="InputManager" /> without a driver container will cause the events
+        /// to be useless and never be raised, only the methods on this class will return any useful info.
+        /// If you want event support, construct the InputManager with a <see cref="GameWindow" />
+        /// or other supported driver providers (NYI).
+        /// </remarks>
+        public InputManager()
+        {
+            // We're assigning an empty mouse device.
+            // This will make position functions return a constant 0.
+            // Instead of being null and causing exceptions.
+            _mouse = new MouseDevice();
+            // We don't have to assign an empty keyboard device,
+            // since we don't have any code that directly relies on it being present.
+        }
+
+        /// <summary>
         /// Creates a new <see cref="InputManager" /> associated with the specified <see cref="GameWindow" />.
         /// </summary>
         /// <param name="window">The <see cref="GameWindow" /> this <see cref="InputManager" /> will interface with.</param>
         public InputManager(GameWindow window)
         {
+            _keyboard = window.Keyboard;
             _mouse = window.Mouse;
+            _keyboard.KeyDown += OnKeyDown;
+            _keyboard.KeyUp += OnKeyUp;
+            window.KeyPress += OnKeyPress;
+            _mouse.ButtonDown += OnMouseDown;
+            _mouse.ButtonUp += OnMouseUp;
+            _mouse.WheelChanged += OnMouseWheelChanged;
+        }
+
+        private void OnKeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            var func = KeyDown;
+            if (func != null)
+                func(this, new KeyEventArgs(e.Key));
+        }
+
+        private void OnKeyUp(object sender, KeyboardKeyEventArgs e)
+        {
+            var func = KeyUp;
+            if (func != null)
+                func(this, new KeyEventArgs(e.Key));
+        }
+
+        private void OnKeyPress(object sender, KeyPressEventArgs e)
+        {
+            var func = KeyPress;
+            if (func != null)
+                func(this, new KeyCharEventArgs(e.KeyChar));
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var func = MouseDown;
+            if (func != null)
+                func(this, e);
+        }
+
+        private void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var func = MouseUp;
+            if (func != null)
+                func(this, e);
+        }
+
+        private void OnMouseWheelChanged(object sender, MouseWheelEventArgs e)
+        {
+            var changeFunc = WheelChanged;
+            var downFunc = WheelDown;
+            var upFunc = WheelUp;
+
+            var delta = e.DeltaPrecise;
+
+            if (delta < 0 && downFunc != null)
+                downFunc(this, e);
+            else if (delta > 0 && upFunc != null)
+                upFunc(this, e);
+
+            if (changeFunc != null)
+                changeFunc(this, e);
         }
 
         public void Update()
@@ -72,12 +192,12 @@ namespace TriDevs.TriEngine2D.Input
             _mouseState = Mouse.GetState();
         }
 
-        public bool KeyUp(Key key)
+        public bool IsKeyUp(Key key)
         {
             return !_keyboardState[key];
         }
 
-        public bool KeyDown(Key key)
+        public bool IsKeyDown(Key key)
         {
             return _keyboardState[key];
         }
@@ -92,37 +212,37 @@ namespace TriDevs.TriEngine2D.Input
             return !_keyboardState[key] && _lastKeyboardState[key];
         }
 
-        public bool ButtonUp(MouseButton button)
+        public bool IsMouseUp(MouseButton button)
         {
             return !_mouseState[button];
         }
 
-        public bool ButtonDown(MouseButton button)
+        public bool IsMouseDown(MouseButton button)
         {
             return _mouseState[button];
         }
 
-        public bool ButtonPressed(MouseButton button)
+        public bool MousePressed(MouseButton button)
         {
             return _mouseState[button] && !_lastMouseState[button];
         }
 
-        public bool ButtonReleased(MouseButton button)
+        public bool MouseReleased(MouseButton button)
         {
             return !_mouseState[button] && _lastMouseState[button];
         }
 
-        public bool WheelUp()
+        public bool IsWheelUp()
         {
             return _mouseState.Wheel > _lastMouseState.Wheel;
         }
 
-        public bool WheelDown()
+        public bool IsWheelDown()
         {
             return _mouseState.Wheel < _lastMouseState.Wheel;
         }
 
-        public bool WheelChanged()
+        public bool IsWheelChanged()
         {
             return _mouseState.Wheel != _lastMouseState.Wheel;
         }
