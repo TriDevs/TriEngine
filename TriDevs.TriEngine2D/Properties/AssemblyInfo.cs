1ef9347c9dd14d4e99b5d2030e2e441c449962b0
Sharparam <adam.hellberg95@gmail.com>
2013-02-17 21:46:39 +0100

    Added exception stuff.

--- TriDevs.TriEngine2D/Properties/AssemblyInfo.cs
+++ TriDevs.TriEngine2D/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -32,4 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.66.1744")]
+[assembly: AssemblyFileVersion("1.0.72.2111")]
+
+// Allow Test project to access internal members
+[assembly: InternalsVisibleTo("TriDevs.TriEngine2D.Tests")]
