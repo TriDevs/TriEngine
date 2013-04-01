e723b1d710dbfd294b66a68f3d13282c40bf12ff
Adam Hellberg <adam.hellberg95@gmail.com>
2013-04-01 02:13:13 +0200

    Documentation update

---
+++ docs/html/search/files_64.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['decimalextensions_2ecs',['DecimalExtensions.cs',['../_decimal_extensions_8cs.html',1,'']]],
+  ['doubleextensions_2ecs',['DoubleExtensions.cs',['../_double_extensions_8cs.html',1,'']]]
+];
